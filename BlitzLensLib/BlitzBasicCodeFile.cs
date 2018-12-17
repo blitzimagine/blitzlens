7bc9adb16521e2d16d902d1ef93b674a369f0d04
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-12-16 21:08:57 -0500

    Parse BBC File

--- BlitzLensLib/BlitzBasicCodeFile.cs
+++ BlitzLensLib/BlitzBasicCodeFile.cs
@@ -9,11 +9,16 @@ namespace BlitzLensLib
 {
 	public class BlitzBasicCodeFile
 	{
-		private byte[] _rawCode;
+		protected byte[] RawCode;
+		protected Dictionary<string, uint> Symbols;
+		protected Dictionary<uint, string> RelativeRelocs;
+		protected Dictionary<uint, string> AbsoluteRelocs;
 
 		private BlitzBasicCodeFile()
 		{
-			
+			Symbols = new Dictionary<string, uint>();
+			RelativeRelocs = new Dictionary<uint, string>();
+			AbsoluteRelocs = new Dictionary<uint, string>();
 		}
 
 		public static BlitzBasicCodeFile FromBytes(byte[] bytes)
@@ -32,8 +37,39 @@ namespace BlitzLensLib
 
 		private bool Read(BinaryReader br)
 		{
-			// TODO: Implement
-			return false;
+			try
+			{
+				int codeSize = br.ReadInt32();
+				RawCode = br.ReadBytes(codeSize);
+
+				int symbolCount = br.ReadInt32();
+				for (int i = 0; i < symbolCount; i++)
+				{
+					Symbols.Add(br.ReadCString(), br.ReadUInt32());
+				}
+
+				int relRelocCount = br.ReadInt32();
+				for (int i = 0; i < relRelocCount; i++)
+				{
+					string sym = br.ReadCString();
+					uint offset = br.ReadUInt32();
+					RelativeRelocs.Add(offset, sym);
+				}
+
+				int absRelocCount = br.ReadInt32();
+				for (int i = 0; i < absRelocCount; i++)
+				{
+					string sym = br.ReadCString();
+					uint offset = br.ReadUInt32();
+					AbsoluteRelocs.Add(offset, sym);
+				}
+			}
+			catch (IOException)
+			{
+				return false;
+			}
+
+			return true;
 		}
 
 		public byte[] GetCode()
---
+++ BlitzLensLib/IOExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlitzLensLib
+{
+	public static class IOExtensions
+	{
+		public static string ReadCString(this BinaryReader br)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			while (!br.Eof())
+			{
+				byte b = br.ReadByte();
+				if (b == 0)
+					break;
+				sb.Append((char)b);
+			}
+
+			return sb.ToString();
+		}
+
+		public static bool Eof(this Stream s)
+		{
+			return s.Position >= s.Length;
+		}
+
+		public static bool Eof(this BinaryReader br)
+		{
+			return br.BaseStream.Eof();
+		}
+	}
+}
