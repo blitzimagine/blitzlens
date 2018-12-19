4986ef440b04cc55922c1019a9ee9430fdf452b4
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-12-18 21:35:14 -0500

    Started work on Decompiler

--- BlitzLensLib/Decompilers/BlitzDecompiler.cs
+++ BlitzLensLib/Decompilers/BlitzDecompiler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BlitzLensLib.Structures;
+using BlitzLensLib.Utils;
 
 namespace BlitzLensLib.Decompilers
 {
@@ -12,15 +13,42 @@ namespace BlitzLensLib.Decompilers
 		protected BlitzLens BlitzLens;
 		protected BlitzDisassembler Disassembler;
 
+		protected Dictionary<string, string> DecompiledCode;
+
 		public BlitzDecompiler(BlitzLens lens, BlitzDisassembler disassembler)
 		{
+			DecompiledCode = new Dictionary<string, string>();
+
 			BlitzLens = lens;
 			Disassembler = disassembler;
 		}
 
 		public void Decompile()
 		{
-			// TODO: Decompile back to BlitzBasic
+			List<uint> offsets = Disassembler.GetDisassembly().Keys.ToList();
+			List<string> instructions = Disassembler.GetDisassembly().Values.ToList();
+
+			string currentFunction = null;
+			string currentLabel = null;
+
+			for (int i = 0; i < Disassembler.GetDisassembly().Count; i++)
+			{
+				uint offset = offsets[i];
+				string instruction = instructions[i];
+
+				string label = Disassembler.GetCode().GetSymbolName(offset);
+				if (label != null)
+				{
+					currentLabel = label;
+					if (label.StartsWith("_f") || label == "__MAIN")
+					{
+						currentFunction = label;
+						Logger.Info(currentFunction.Indent());
+					}
+				}
+
+				// TODO: Decompile back to BlitzBasic
+			}
 		}
 	}
 }
